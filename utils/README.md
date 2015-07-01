894fffaf5911dc7cb35b6a7c9222baab40d30617
cayetanobv <cayetano.benavent@geographica.gs>
2015-07-01 16:16:52 +0200

    utilities folder added - SQL scripts for DB creation

---
+++ utils/README.md
@@ -0,0 +1,5 @@
+## Utils
+
+Scripts and utilities:
+- SQL (PgSQL/PostGIS).
+- etc.
---
+++ utils/sql/redlist_create_db.sql
@@ -0,0 +1,30 @@
+-- Role: redlist_admin
+
+-- DROP ROLE redlist_admin;
+
+CREATE ROLE redlist_admin LOGIN
+  NOSUPERUSER INHERIT NOCREATEDB NOCREATEROLE NOREPLICATION;
+
+-- Database: redlist
+
+-- DROP DATABASE redlist;
+
+CREATE DATABASE redlist
+  WITH OWNER = redlist_admin
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'C'
+       LC_CTYPE = 'C'
+       CONNECTION LIMIT = -1;
+
+-- Connect to Red List DB
+\connect redlist
+
+-- Create Schema to store map layers
+CREATE SCHEMA layers;
+ALTER SCHEMA layers OWNER TO redlist_admin;
+
+-- Enable PostGIS (includes raster)
+CREATE EXTENSION postgis;
+-- Enable Topology
+CREATE EXTENSION postgis_topology;
