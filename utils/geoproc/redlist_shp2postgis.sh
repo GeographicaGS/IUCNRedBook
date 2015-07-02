6d6eaa870b526cc3354349114dbe29cdf417e9ab
cayetanobv <cayetano.benavent@geographica.gs>
2015-07-02 17:45:49 +0200

    Shapefile to PostGIS scripts

---
+++ utils/geoproc/redlist_shp2postgis.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+#
+#  Author: Cayetano Benavent, 2015.
+#  https://github.com/GeographicaGS/IUCNRedBook
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+CURRDIR=$(pwd)
+. $CURRDIR/CONFIGFILE
+
+for fl in $GEODATAFOLDER/*.shp;
+  do
+    (
+        for f in $fl;
+            do
+              (
+                printf '\n' && echo $f
+                shp=$(basename $f .shp)
+                shp2pgsql -s $CRS_EPSG $f $DBSCHEMA.$shp >> $SQLFILE
+              )
+            done
+      );
+  done
+
+  psql -h $HOST -d $DATABASE -p $PORT -U $USER -f $SQLFILE
+
+  $(rm $SQLFILE)
