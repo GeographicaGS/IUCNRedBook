15b8e83736e6f6a0fa8b4ebfd0869076531c9973
cayetanobv <cayetano.benavent@geographica.gs>
2015-07-03 18:51:19 +0200

    added geom index generator for Postgis layers

--- utils/sql/redlist_unionlayers.sql
+++ utils/sql/redlist_unionlayers.sql
@@ -13,18 +13,3 @@ CREATE INDEX mo_assessment_multi_gist
 
 ALTER TABLE layers_flt.mo_assessment_multi
     OWNER TO redlist_admin;
-
-
--- LAYER: mo_assessment9808-250_flt
-DROP TABLE IF EXISTS layers_flt.mo_assessment_multi;
-
-CREATE TABLE layers_flt.mo_assessment_multi AS
-    SELECT ST_Multi(ST_Union(geom)) as geom, values
-        FROM layers."mo_assessment9808-250_flt"
-	GROUP BY values;
-
-CREATE INDEX mo_assessment_multi_gist
-    ON  layers_flt.mo_assessment_multi USING GIST (geom);
-
-ALTER TABLE layers_flt.mo_assessment_multi
-    OWNER TO redlist_admin;
